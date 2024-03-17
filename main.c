0946d2862f3f5dac2bb03c82e2a9802e2f344624
dirichy <144102919+dirichy@users.noreply.github.com>
2024-03-17 23:08:16 +0800

    update

--- main.c
+++ main.c
@@ -1,62 +1,98 @@
 #include<stdio.h>
 #include<stdlib.h>
+/// the type gint is to restore a great number. 
+/// Although now it's only long long, it will be better in the future
 typedef struct {
-  int value;
+  long long value;
 } gint;
-
+/// this function is to display a gint
 void gdisplay(gint a){
-  printf("%d",a.value);
+  printf("%lld",a.value);
 }
-
-gint gadd(gint a,gint b){
-  gint c;
-  c.value=a.value+b.value;
-  return c;
-}
-
-gint gminus(gint a,gint b){
-  gint c;
-  c.value=a.value-b.value;
-  return c;
-}
-gint gmultiply(gint a,gint b){
-  gint c;
-  c.value=a.value*b.value;
-  return c;
-}
-gint gdivide(gint a,gint b){
-  gint c;
-  c.value=a.value/b.value;
-  return c;
-}
-gint gmod(gint a,gint b){
-  gint c;
-  c.value=a.value%b.value;
-  return c;
-}
-gint grandom(){
-  gint a;
-  a.value=rand();
-  return a;
-}
-int gisprime(gint a){
-  int b=a.value;
-  if(b==1) return 0;
-  for(int i=2;i<b;i++){
-    if(b%i==0) return 0;
-  }
-  return 1;
-}
+/// this function is to convert a int into gint
 gint int2gint(int a){
   gint b;
   b.value=a;
   return b;
 }
+///this function is to add two gint
+gint gadd(gint a,gint b){
+  gint c;
+  c.value=a.value+b.value;
+  return c;
+}
+///this function is to minus two gint
+gint gminus(gint a,gint b){
+  gint c;
+  c.value=a.value-b.value;
+  return c;
+}
+///this function is to multiply two gint
+gint gmultiply(gint a,gint b){
+  gint c;
+  c.value=a.value*b.value;
+  return c;
+}
+///this function is to divide two gint
+gint gdivide(gint a,gint b){
+  gint c;
+  c.value=a.value/b.value;
+  return c;
+}
+///this function is to mod two gint
+gint gmod(gint a,gint b){
+  gint c;
+  c.value=a.value%b.value;
+  return c;
+}
+/// this function is to do the fast power mod n for gint. 
+/// will return a^b mod n
+gint gmodpower(gint n,gint a,gint b){
+  long long c=a.value%n.value;
+  long long d=b.value;
+  long long s=1;
+  while(d){
+    if(d&1) s=s*c%n.value;
+    d=d >> 1;
+    c=c*c%n.value;
+  }
+  gint t=int2gint(s);
+  return t;
+}
+/// this function is to genetate a random gint
+gint grandom(){
+  gint a;
+  a.value=rand();
+  return a;
+}
+/// this function is to judge whether a gint is a prime 
+int gisprime(gint a){
+  int b=a.value;
+  if(b<=1) return 0;
+  for(long long i=2;i*i<=b;i++){
+    if(b%i==0) return 0;
+  }
+  return 1;
+}
+/// this function is to add a gint and an int
 gint gintaddint(gint a,int b){
   gint c;
   c.value=a.value+b;
   return c;
 }
+/// this function is to minus a gint by an int
+gint gintminusint(gint a,int b){
+  gint c;
+  c.value=a.value-b;
+  return c;
+}
+/// this function returns (gint)0
+gint gzero(){
+  gint a;
+  a.value=0;
+  return a;
+}
+/// this function is to genetate a randomprime
 gint randomprime(){
   gint a;
   while(a.value<9999){
@@ -67,13 +103,22 @@ gint randomprime(){
   }
   return a;
 }
+/// this function is to solve the eqution ed = 1 mod n 
 gint ginverse(gint n,gint e){
   gint d;
-//ed \equiv 1 \mod n
+  //ed \equiv 1 \mod n
+  //if gcd(e,n)!=1 then return (gint)0;
   return d;
 }
+/// this function is to do the encode or decode
+/// will return message^secret mod n
+gint gcode(gint n,gint message,gint secret){
+  return gmodpower(n,message,secret);
+}
+
 int main(){
-  gint a=randomprime();
-  gdisplay(a);
+  gdisplay(gmodpower(int2gint(999999),int2gint(89898989),int2gint(178990000)));
+  // gint a=randomprime();
+  // gdisplay(a);
   return 0;
 }
